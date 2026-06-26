d1ce79b7266ab834fdf8b696243c87e8b25016e6
Raffinnaisse <rabbit23113@gmail.com>
2026-06-26 21:25:20 +0300

    chore: initial version of auth service

---
+++ apps/authMicroservice/src/main.ts
@@ -0,0 +1,31 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['kafka:9092'],
+          clientId: 'auth-service',
+        },
+        consumer: {
+          groupId: 'auth-consumer-group',
+        },
+      },
+    },
+  );
+  await app.listen();
+  Logger.log(`AUTH MICROSERVICE IS RUNNING`);
+}
+
+bootstrap();
