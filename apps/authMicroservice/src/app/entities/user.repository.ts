5db4b04ce7d10fe855cbb18e4fcb4168af1599e7
Raffinnaisse <rabbit23113@gmail.com>
2026-06-26 22:01:33 +0300

    chore: user repository

---
+++ apps/authMicroservice/src/app/entities/user.repository.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+enum UserStatus {
+  PENDING = 'pending',
+  ACTIVE = 'active',
+  BLOCKED = 'blocked',
+}
+
+@Entity()
+export class UserRepository {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'user_id', type: 'uuid', default: () => 'gen_random_uuid()' })
+  userId: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ name: 'password_hash' })
+  passwordHash: string;
+
+  @Column({ type: 'simple-enum', enum: UserStatus, default: UserStatus.PENDING })
+  status: UserStatus;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
