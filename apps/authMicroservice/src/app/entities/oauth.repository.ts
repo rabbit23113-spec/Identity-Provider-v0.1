466c62125de53a810a7be6e6bd2ad54cfe7a54ec
Raffinnaisse <rabbit23113@gmail.com>
2026-06-26 22:20:08 +0300

    chore: oauth repository

---
+++ apps/authMicroservice/src/app/entities/oauth.repository.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class OauthRepository {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    name: 'oauth_client_id',
+    type: 'uuid',
+    default: () => 'gen_random_uuid()',
+  })
+  oauthClientId: string;
+
+  @Column({ name: 'client_secret_hash' })
+  clientSecretHash: string;
+
+  @Column()
+  name: string;
+
+  @Column({ name: 'redirect_uris', type: 'simple-array', default: [] })
+  redirectUris: string[];
+
+  @Column({ name: 'grant_types', type: 'simple-array', default: [] })
+  grantTypes: string[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
