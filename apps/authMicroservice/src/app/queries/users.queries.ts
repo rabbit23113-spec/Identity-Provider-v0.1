64b96230971f9225701b9b8df106f59334b22f08
Raffinnaisse <rabbit23113@gmail.com>
2026-06-27 15:04:57 +0300

    feat: user queries

--- apps/authMicroservice/src/app/queries/users.queries.ts
+++ apps/authMicroservice/src/app/queries/users.queries.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserRepository } from '../entities/user.repository';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersQueries {}
+export class UsersQueries {
+  constructor(private readonly userRepository: Repository<UserRepository>) {}
+
+  async findOne(userId: string): Promise<UserRepository> {
+    const target = await this.userRepository.findOneBy({ userId });
+    if (!target) {
+      throw new NotFoundException(`User with id ${userId} not found`);
+    }
+    return target;
+  }
+
+  async findOneByEmail(email: string): Promise<UserRepository> {
+    const target = await this.userRepository.findOneBy({ email });
+    if (!target) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+    return target;
+  }
+}
