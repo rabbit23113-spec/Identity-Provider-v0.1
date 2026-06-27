d2699966636aef4d2667aff4dc67e43f7946fd4b
Raffinnaisse <rabbit23113@gmail.com>
2026-06-27 15:13:50 +0300

    feat: user commands

--- apps/authMicroservice/src/app/commands/users.commands.ts
+++ apps/authMicroservice/src/app/commands/users.commands.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { UserRepository } from '../entities/user.repository';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/users/createUser.dto';
+import { OutboxRepository } from '../entities/outbox.repository';
 
 @Injectable()
-export class UsersCommands {}
+export class UsersCommands {
+  constructor(
+    private readonly userRepository: Repository<UserRepository>,
+    private readonly outboxRepository: Repository<OutboxRepository>,
+  ) {}
+
+  async createOne(dto: CreateUserDto): Promise<UserRepository> {
+    try {
+      const user: UserRepository = this.userRepository.create(dto);
+      await this.userRepository.save(user);
+      const event = this.outboxRepository.create({
+        eventType: 'users.created',
+        payload: user,
+      });
+      await this.outboxRepository.save(event);
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
