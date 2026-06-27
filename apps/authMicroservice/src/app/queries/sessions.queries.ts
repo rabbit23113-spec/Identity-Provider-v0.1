73557b7099726afda7e6336b34821c59264f9b71
Raffinnaisse <rabbit23113@gmail.com>
2026-06-27 15:19:43 +0300

    feat: session queries

--- apps/authMicroservice/src/app/queries/sessions.queries.ts
+++ apps/authMicroservice/src/app/queries/sessions.queries.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { SessionRepository } from '../entities/session.repository';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SessionsQueries {}
+export class SessionsQueries {
+  constructor(
+    private readonly sessionRepository: Repository<SessionRepository>,
+  ) {}
+
+  async findAll(): Promise<SessionRepository[]> {
+    return this.sessionRepository.find();
+  }
+
+  async findOne(sessionId: string): Promise<SessionRepository> {
+    const target: SessionRepository | null =
+      await this.sessionRepository.findOneBy({ sessionId });
+    if (!target) {
+      throw new NotFoundException(`Session with id ${sessionId} not found`);
+    }
+    return target;
+  }
+}
