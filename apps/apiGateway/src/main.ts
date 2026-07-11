197e0315a2c67017d3cf1c2efb3d91cc7c596baa
Raffinnaisse <rabbit23113@gmail.com>
2026-07-11 10:30:25 +0300

    chore: swagger in main.ts

--- apps/apiGateway/src/main.ts
+++ apps/apiGateway/src/main.ts
@@ -6,11 +6,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('Identity Provider v0.1')
+    .setDescription('This is a documentation of the API')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
