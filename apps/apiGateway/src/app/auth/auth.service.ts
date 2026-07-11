34814bf14d9d217e0535581b9076aa073f0becb8
Raffinnaisse <rabbit23113@gmail.com>
2026-07-11 10:06:26 +0300

    feat: basic routes

--- apps/apiGateway/src/app/auth/auth.service.ts
+++ apps/apiGateway/src/app/auth/auth.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { constants } from '../constants/app.constants';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(constants.KAFKA_CLIENT) private readonly clientKafka: ClientKafka,
+  ) {}
+
+  async register() {}
+  async login() {}
+  async revoke() {}
+  async rotate() {}
+
+  async oauthGenUrl() {}
+  async oauthCallback() {}
+
+}
